3d492d15afac15ee35cb48cd7cc58905c0d567a2
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-13 20:41:42 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,14 +3,19 @@ class Backer
   attr_reader :name
   attr_accessor :backed_projects
   
+  @@all = []
+  
   def initialize(name)
     @name = name
     @backed_projects = []
+    @@all << self
   end
   
   def back_project(project)
     self.backed_projects << project
-    Project
+    Project.all.each do |proj|
+      if project == proj
+        project.a
   end
   
 end
