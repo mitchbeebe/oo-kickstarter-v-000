6a41f396a2a818073eaacc2e4f2b4e66f501608e
Mitch Beebe <mitchell.beebe@flatironschool.com>
2019-11-13 20:40:18 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -3,9 +3,12 @@ class Project
   attr_reader :title
   attr_accessor :backers
   
+  @@all = []
+  
   def initialize(title)
     @title = title
     @backers = []
+    @@all << self
   end
   
   def add_backer(backer)
